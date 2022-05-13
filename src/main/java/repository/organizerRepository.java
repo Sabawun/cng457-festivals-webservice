6849718577a1de6da168ba4b1bd3d7020e835bce
Sabawun <67931515+Sabawun@users.noreply.github.com>
2022-05-13 23:22:28 +0300

    Delete src/main/java/repository directory

--- src/main/java/repository/organizerRepository.java
+++
@@ -1,8 +0,0 @@
-package repository;
-
-import entities.organizer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface organizerRepository extends JpaRepository<organizer, Integer> {
-
-}
